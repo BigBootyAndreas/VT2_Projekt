b3a3784d26e0b9c2e41955591cca01a29540c677
Ali <114579912+BigBootyAndreas@users.noreply.github.com>
2025-03-11 12:42:21 +0100

    csv reader is optimized and done. Is also integrated into the interface

--- File_reader.py
+++ File_reader.py
@@ -1,12 +1,12 @@
-import os
-import csv
+import pandas as pd
 
 def read_csv_file(file_path):
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            print(f"\nReading file: {os.path.basename(file_path)}\n")
-            reader = csv.reader(file)
-            for row in reader:
-                print(row)  # Print each row of the CSV file
+        # Read the CSV file into a DataFrame
+        df = pd.read_csv(file_path,usecols=[1,2],skiprows=1)
+    
+        # Print the filtered rows
+        print(df)
+
     except Exception as e:
         print(f"Error reading file: {e}")

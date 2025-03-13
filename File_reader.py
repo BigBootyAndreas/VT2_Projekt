b8520dc690191947d0acebe66fa3e39ee6bd3826
Ali <114579912+BigBootyAndreas@users.noreply.github.com>
2025-03-13 10:35:11 +0100

    fix

--- File_reader.py
+++ File_reader.py
@@ -3,11 +3,11 @@ import pandas as pd
 def read_csv_file(file_path, folder_choice):
     try:
         if folder_choice == '1':
-            # For IMU Data, read specific columns (X, Y, Z, Time) and skip the first row
-            df = pd.read_csv(file_path, skiprows=1)
+            # For IMU Data skip the first row
+            df = pd.read_csv(file_path)
         elif folder_choice == '2':
             # For Acoustic Data, read different columns
-            df = pd.read_csv(file_path, usecols=[1,2])
+            df = pd.read_csv(file_path,skiprows=[0], usecols=[1,2])
         else:
             print("Invalid folder choice.")
             return None  # Return None if the folder choice is invalid
